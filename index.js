bedd1b5fc1a42456a49871a78099b4c87a11b28d
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-23 16:31:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ function preventRefreshOnSubmit(){
       event.preventDefault()
     })
 }
+function retrieveEmployeeInformation() {
+  return $('input[name="name"]').get(0).value;
+}
+function addNewElementAsLi(value) {
+  $('.employee-list').get(0).append(`<li>${value}</li>`);
+}
+
 
 preventRefreshOnSubmit()
